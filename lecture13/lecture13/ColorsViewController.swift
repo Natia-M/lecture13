951079ecc0c8908a108783ee45f12cd3d09a77f2
NatiaMinasiani <natiaminasiani@gmail.com>
2026-05-10 17:05:26 +0400

    created main fails

---
+++ lecture13/lecture13/ColorsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ColorsViewController.swift
+//  lecture13
+//
+//  Created by naat minasiani on 10/05/2026.
+//
+
+import Foundation
---
+++ lecture13/lecture13/lecture13App.swift
@@ -0,0 +1,17 @@
+//
+//  lecture13App.swift
+//  lecture13
+//
+//  Created by naat minasiani on 10/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct lecture13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
