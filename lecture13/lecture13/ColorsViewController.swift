c6c990d79792a7633cf481087e01ac363c78cffa
NatiaMinasiani <natiaminasiani@gmail.com>
2026-05-10 17:29:16 +0400

    added details

--- lecture13/lecture13/ColorsViewController.swift
+++ lecture13/lecture13/ColorsViewController.swift
@@ -6,3 +6,70 @@
 //
 
 import Foundation
+import UIKit
+
+class ColorsViewController: UIViewController {
+
+    let redView = UIView()
+    let blueView = UIView()
+    let greenView = UIView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+
+        setupRedView()
+        setupBlueView()
+        setupGreenView()
+    }
+
+    private func setupRedView() {
+
+        view.addSubview(redView)
+
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        redView.backgroundColor = .red
+
+        NSLayoutConstraint.activate([
+            redView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            redView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            redView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            redView.heightAnchor.constraint(equalToConstant: 250)
+        ])
+    }
+
+    private func setupBlueView() {
+
+        view.addSubview(blueView)
+
+        blueView.translatesAutoresizingMaskIntoConstraints = false
+        blueView.backgroundColor = .systemBlue
+
+        NSLayoutConstraint.activate([
+            blueView.topAnchor.constraint(equalTo: redView.bottomAnchor, constant: 20),
+            blueView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            blueView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            blueView.heightAnchor.constraint(equalTo: redView.heightAnchor)
+        ])
+    }
+
+    private func setupGreenView() {
+
+        view.addSubview(greenView)
+
+        greenView.translatesAutoresizingMaskIntoConstraints = false
+        greenView.backgroundColor = .systemGreen
+
+        NSLayoutConstraint.activate([
+            greenView.topAnchor.constraint(equalTo: blueView.bottomAnchor, constant: 20),
+            greenView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            greenView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            greenView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+#Preview {
+    ColorsViewController()
+}
+
--- lecture13/lecture13/ProfileViewController.swift
+++ lecture13/lecture13/ProfileViewController.swift
@@ -10,155 +10,264 @@ import Foundation
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
     let backgroundImageView = UIImageView()
     let profileImageView = UIImageView()
-
+    
     let nameLabel = UILabel()
     let professionLabel = UILabel()
     let emojiLabel = UILabel()
-
+    
     let cameraButton = UIButton()
     let closeButton = UIButton()
     let heartButton = UIButton()
     let linkButton = UIButton()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .systemGray6
-
+        
         setupBackgroundImage()
         setupProfileImage()
         setupLabels()
         setupButtons()
     }
-
+    
     private func setupBackgroundImage() {
-
+        
         view.addSubview(backgroundImageView)
-
+        
         backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
-
+        
         NSLayoutConstraint.activate([
-            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
-            backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            backgroundImageView.heightAnchor.constraint(equalToConstant: 300)
+            
+            backgroundImageView.topAnchor.constraint(
+                equalTo: view.topAnchor
+            ),
+            
+            backgroundImageView.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor
+            ),
+            
+            backgroundImageView.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor
+            ),
+            
+            backgroundImageView.heightAnchor.constraint(
+                equalToConstant: 300
+            )
         ])
-
-        backgroundImageView.image = UIImage(named: "background")
+        
+        backgroundImageView.image = UIImage(named: "bulding")
         backgroundImageView.contentMode = .scaleAspectFill
         backgroundImageView.clipsToBounds = true
     }
-
+  
     private func setupProfileImage() {
-
+        
         view.addSubview(profileImageView)
-
+        
         profileImageView.translatesAutoresizingMaskIntoConstraints = false
-
+        
         NSLayoutConstraint.activate([
-            profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            profileImageView.topAnchor.constraint(equalTo: backgroundImageView.bottomAnchor, constant: -90),
-            profileImageView.widthAnchor.constraint(equalToConstant: 180),
-            profileImageView.heightAnchor.constraint(equalToConstant: 180)
+            
+            profileImageView.centerXAnchor.constraint(
+                equalTo: view.centerXAnchor
+            ),
+            
+            profileImageView.topAnchor.constraint(
+                equalTo: backgroundImageView.bottomAnchor,
+                constant: -95
+            ),
+            
+            profileImageView.widthAnchor.constraint(
+                equalToConstant: 180
+            ),
+            
+            profileImageView.heightAnchor.constraint(
+                equalToConstant: 180
+            )
         ])
-
-        profileImageView.image = UIImage(named: "steve")
+        
+        profileImageView.image = UIImage(named: "stive")
+        
         profileImageView.contentMode = .scaleAspectFill
         profileImageView.clipsToBounds = true
-
+        
         profileImageView.layer.cornerRadius = 90
-        profileImageView.layer.borderWidth = 6
+        profileImageView.layer.borderWidth = 5
         profileImageView.layer.borderColor = UIColor.systemPurple.cgColor
     }
-
+    
+    // MARK: - Labels
+    
     private func setupLabels() {
+
+        // MARK: - Name Label
 
         view.addSubview(nameLabel)
 
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 30),
-            nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+
+            nameLabel.topAnchor.constraint(
+                equalTo: profileImageView.bottomAnchor,
+                constant: 30
+            ),
+
+            nameLabel.centerXAnchor.constraint(
+                equalTo: view.centerXAnchor
+            )
         ])
 
         nameLabel.text = "სახელი გვარი"
-        nameLabel.font = .systemFont(ofSize: 32, weight: .bold)
+
+        nameLabel.font = UIFont.systemFont(
+            ofSize: 32,
+            weight: .bold
+        )
+
+        nameLabel.textColor = .black
+
+
+
+        // MARK: - Profession Label
 
         view.addSubview(professionLabel)
 
         professionLabel.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            professionLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 10),
-            professionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+
+            professionLabel.topAnchor.constraint(
+                equalTo: nameLabel.bottomAnchor,
+                constant: 10
+            ),
+
+            professionLabel.centerXAnchor.constraint(
+                equalTo: view.centerXAnchor
+            )
         ])
 
         professionLabel.text = "@iOS Dev"
+
         professionLabel.textColor = .gray
-        professionLabel.font = .systemFont(ofSize: 24)
+
+        professionLabel.font = UIFont.systemFont(
+            ofSize: 24,
+            weight: .regular
+        )
+
+
+
+        // MARK: - Emoji Label
 
         view.addSubview(emojiLabel)
 
         emojiLabel.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            emojiLabel.topAnchor.constraint(equalTo: professionLabel.bottomAnchor, constant: 10),
-            emojiLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+
+            emojiLabel.topAnchor.constraint(
+                equalTo: professionLabel.bottomAnchor,
+                constant: 10
+            ),
+
+            emojiLabel.centerXAnchor.constraint(
+                equalTo: view.centerXAnchor
+            )
         ])
 
-        emojiLabel.text = "🎵 🏀 📚"
-        emojiLabel.font = .systemFont(ofSize: 40)
+        emojiLabel.text = "♫ ⛹︎ 📖"
+        emojiLabel.font = UIFont.systemFont(ofSize: 35)
+
+        emojiLabel.textAlignment = .center
     }
-
     private func setupButtons() {
-
+        
         let stackView = UIStackView()
-
+        
         view.addSubview(stackView)
-
+        
         stackView.translatesAutoresizingMaskIntoConstraints = false
-
+        
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: emojiLabel.bottomAnchor, constant: 50),
-            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
-            stackView.heightAnchor.constraint(equalToConstant: 80)
+            
+            stackView.topAnchor.constraint(
+                equalTo: emojiLabel.bottomAnchor,
+                constant: 50
+            ),
+            
+            stackView.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: 30
+            ),
+            
+            stackView.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: -30
+            ),
+            
+            stackView.heightAnchor.constraint(
+                equalToConstant: 70
+            )
         ])
-
+        
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
         stackView.spacing = 20
-
-        setupButtonStyle(button: cameraButton, imageName: "camera")
-        setupButtonStyle(button: closeButton, imageName: "xmark")
-        setupButtonStyle(button: heartButton, imageName: "heart")
-        setupButtonStyle(button: linkButton, imageName: "link")
-
+        
+        setupButtonStyle(
+            button: cameraButton,
+            imageName: "camera"
+        )
+        
+        setupButtonStyle(
+            button: closeButton,
+            imageName: "xmark"
+        )
+        
+        setupButtonStyle(
+            button: heartButton,
+            imageName: "heart"
+        )
+        
+        setupButtonStyle(
+            button: linkButton,
+            imageName: "link"
+        )
+        
         stackView.addArrangedSubview(cameraButton)
         stackView.addArrangedSubview(closeButton)
         stackView.addArrangedSubview(heartButton)
         stackView.addArrangedSubview(linkButton)
     }
-
-    private func setupButtonStyle(button: UIButton, imageName: String) {
-
+    
+    private func setupButtonStyle(
+        button: UIButton,
+        imageName: String
+    ) {
+        
         button.backgroundColor = .systemGray3
-
+        
         button.layer.cornerRadius = 20
-
-        button.setImage(
-            UIImage(systemName: imageName),
-            for: .normal
+        
+        let image = UIImage(
+            systemName: imageName
+        )?.withConfiguration(
+            UIImage.SymbolConfiguration(
+                pointSize: 28,
+                weight: .medium
+            )
         )
-
+        
+        button.setImage(image, for: .normal)
+        
         button.tintColor = .black
     }
 }
-
 #Preview {
     ProfileViewController()
 }
